9e3620e97a95b7992df191e1c045e27c34f44bf9
Bernard Stevens <bernard.stevens@free.fr>
2023-11-14 14:32:57 +0100

    Update envs.go

--- initializers/envs.go
+++ initializers/envs.go
@@ -1,14 +1,4 @@
 package initializers
 
-import (
-	"github.com/gobuffalo/envy"
-	"github.com/rs/zerolog/log"
-)
-
 // InitializeEnvs intializes envy
-func InitializeEnvs() {
-	if err := envy.Load("./.env"); err != nil {
-		log.Warn().Err(err).Msg("cannot load .env file")
-		envy.Reload()
-	}
-}
+func InitializeEnvs() {}
